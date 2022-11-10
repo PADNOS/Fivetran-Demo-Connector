4d2f9be09cb9a64cd7d6105d8bb1cd8f210c703d
Ernie <34193363+erniefrs@users.noreply.github.com>
2022-11-10 11:03:47 -0500

    Add data logic

--- Connector.cs
+++ Connector.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -21,7 +23,29 @@ namespace FivetranDemoConnector
       // Initialize context
       var context = await FivetranContext.FromRequestBodyAsync(req.Body);
 
+      var apiClient = new HttpClient();
 
+      // Create and populate forecast table
+      var forecastTable = new Table<dynamic>("forecast");
+      var forecastData = await apiClient.GetFromJsonAsync<JsonElement>("https://weather.padnos.com/api/forecasts");
+
+      foreach (var forecast in forecastData.EnumerateArray())
+      {
+        forecastTable.CurrentItems!.Add(forecast);
+      }
+
+      // Create and populate storm event table
+      var eventTable = new Table<dynamic>("storm_event");
+      var stormEventData = await apiClient.GetFromJsonAsync<JsonElement>("https://weather.padnos.com/api/events");
+
+      foreach (var stormEvent in stormEventData.EnumerateArray())
+      {
+        eventTable.CurrentItems!.Add(stormEvent);
+      }
+
+      // Populate context
+      context.Tables.Add(forecastTable);
+      context.Tables.Add(eventTable);
 
       // Build response
       var response = req.CreateResponse(HttpStatusCode.OK);
